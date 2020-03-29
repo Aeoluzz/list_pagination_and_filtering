f40ac6c03e20fa50e853a50c1ea244fe59d830f7
Aeoluzz <scherwainm@outlook.com>
2020-03-29 17:46:02 -0400

    meets expectation

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # list_pagination_and_filtering
  
+# Create pagination by displaying 10 names or less per page and add links for users to click through.
--- js/script.js
+++ js/script.js
@@ -3,97 +3,64 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
    
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
 
 /*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
+  Generate two global variables for the entire project
 ***/
-let list = document.querySelectorAll('ul.student-list')[0].children;
-list = Array.from(list);
-let itemsPerPage = 10;
+let list = document.querySelectorAll('ul.student-list')[0].children; // li elements stored as HTMLCollection
+list = Array.from(list); // convert li elements from HTMLCollection to Array
+let itemsPerPage = 10; 
 
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
-const students = [];
-for (let i=0; i<list.length; i++) {
-   students.push(list[i]);
-}
-// console.log(students);
-
-// let page = 1;
-
+/***
+ * Create the showPage function to determing the amount of list elements to display per page
+ */
 
 function showPage(list, page) {
-   let startIndex = (page * itemsPerPage) - itemsPerPage;
-   let endIndex = page * itemsPerPage;
+   let startIndex = (page * itemsPerPage) - itemsPerPage; // start of items to be displayed on a page at a time
+   let endIndex = page * itemsPerPage; // end of items to be displayed on a page at a time
    for (let i=0; i<list.length; i++) {
-      if (list.indexOf(list[i]) >= startIndex && list.indexOf(list[i]) < endIndex) {
-         console.log(list[i]);
+      if (list.indexOf(list[i]) >= startIndex && list.indexOf(list[i]) < endIndex) { 
+         list[i].style.display = 'block'; // display the list item if the condition is true
+      } else {
+         list[i].style.display = 'none';
       }
    }
 }
 
-// showPage(students, page);
+
 /*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
+   Create the appendPageLinks function to add paginated links to the bottom of the page dynamically
 ***/
 
 
 function appendPageLinks(list) {
-   let maxNumOfPages = Math.ceil(list.length/10);
+   let maxNumOfPages = Math.ceil(list.length/10); // Generate the number of pages
    let div = document.createElement('div'); // create the div element
    div.className = 'pagination'; // add the class pagination to the pagination div element
    let pageDiv = document.querySelector('.page'); // select the parent div
-   
    let ul = document.createElement('ul'); // create the unordered list element
-   // need to create a loop somehow
-   for (var i=0; i<maxNumOfPages; i++) {
+   for (var i=0; i<maxNumOfPages; i++) { // create an li,a element with text for each page
       let li = document.createElement('li');
       let a = document.createElement('a');
-      a.textContent = i+1;
+      a.textContent = i+1; // add 1 to the index count since variable i is initially set to 0
       a.href = '#';
-      li.appendChild(a);
-      ul.appendChild(li);
-   }
-   div.appendChild(ul);
-   pageDiv.appendChild(div);
+      li.appendChild(a); // append the link to the list element
+      ul.appendChild(li); // append the list element to the unordered list
+   } // End for loop
+   div.appendChild(ul); // append the unordered list to the div with the class of pagination
+   pageDiv.appendChild(div);  // append div.pagination to div.page
    ul.firstElementChild.firstElementChild.className = 'active';
-   const allLinks = document.querySelectorAll('.pagination ul a');
+   const allLinks = document.querySelectorAll('.pagination ul a'); // add class of active to first li element
    allLinks.forEach((link) => {
       link.addEventListener('click', (e) => {
          e.preventDefault();
          allLinks.forEach((a) => {
-            a.classList.remove('active');
-         })// remove class active from all links
+            a.classList.remove('active'); // remove class active from all links
+         }); 
          e.target.className = 'active'; // add active class to the link clicked
-         showPage(students, e.target.textContent);
+         showPage(list, e.target.textContent); // execute the showPage function when a li element is clicked
       }); // end click event
-   });
-  
-   // console.log(pageDiv);
+   }); // End forEach loop
 }
 showPage(list, 1);
 appendPageLinks(list);
